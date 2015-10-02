9e4399328846c7e31d0cbcf5fb272bbb1c04991b
Fernando Cancellara <fdcancellara@gmail.com>
2015-10-02 17:02:11 -0300

    Gracias lserni por el gran código de alocación dinámica, se agregan más free.

--- tp0.c
+++ tp0.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #define ARGUMENTS_NEEDED 2
 #define CURRENT_VERSION "1.0"
@@ -13,7 +14,7 @@
 #define ERROR_ODD_NUM_MATRICES 1
 #define ERROR_NOT_MATCHING_ROWCOL 2
 #define ERROR_NOT_MATCHING_ELEMENTS 3
-
+#define BLOCKSIZE 1024
 
 /**
  * parses line by tokens
@@ -36,6 +37,10 @@ bool areCompatible( int firstColumn, int secondRow);
  */
 bool haveCorrectNumberOfElements(int numberElementsExpected, int counter);
 
+/**
+ * used to get an arbitrary line of text
+ */
+char *getWholeLine(FILE * fp);
 
 int main(int argc, char *argv[]) {
 
@@ -44,25 +49,28 @@ int main(int argc, char *argv[]) {
 	}
 	else {
 
-		char buf[BUFSIZ*2];
+		char * buf;
 
 		int counter;
 
 		int firstRow, firstColumn, secondRow, secondColumn;
 
-		while ( fgets(buf, sizeof buf, stdin) != NULL ) {
+		while ( (buf = getWholeLine(stdin)) != NULL ) {
 
 			//parse first line
 			double *firstValues = parseLine( buf, &firstRow, &firstColumn, &counter );
 
 			if (!haveCorrectNumberOfElements( firstRow * firstColumn, counter)) {
-				fprintf( stderr, "ERROR: invalid number of elements in the line. %dx%d must contains %d elements\n",
+				fprintf( stderr, "ERROR: invalid number of elements in the line. %dx%d must contain %d elements\n",
 						firstRow, firstColumn, (firstRow) * (firstColumn) );
+				free(firstValues);
 				return ERROR_NOT_MATCHING_ELEMENTS;
 			}
 
-			if ( fgets(buf, sizeof buf, stdin) == NULL ){
+			if ( (buf = getWholeLine(stdin)) == NULL ){
 			    fprintf(stderr, "ERROR: input file with invalid number of matrices.\n");
+			    free(buf);
+			    free(firstValues);
 			    return ERROR_ODD_NUM_MATRICES;
 
 			} else {
@@ -71,8 +79,11 @@ int main(int argc, char *argv[]) {
 				double *secondValues = parseLine( buf, &secondRow, &secondColumn, &counter );
 
 				if (!haveCorrectNumberOfElements( secondRow * secondColumn, counter )) {
-					fprintf( stderr, "ERROR: invalid number of elements in the line. %dx%d must contains %d elements\n",
+					fprintf( stderr, "ERROR: invalid number of elements in the line. %dx%d must contain %d elements\n",
 							secondRow, secondColumn, (secondRow) * (secondColumn) );
+					free(firstValues);
+					free(secondValues);
+					free(buf);
 					return ERROR_NOT_MATCHING_ELEMENTS;
 				}
 
@@ -115,6 +126,7 @@ int main(int argc, char *argv[]) {
 				free( secondValues );
 			}
 		}
+		free(buf);
 	}
 
 	return 0;
@@ -140,9 +152,10 @@ double* parseLine( char *buf, int *row, int *column, int *counter ) {
 	*counter = 0;
 	while ( (token = strtok(NULL, WHITE_SPACE)) ) {
 		values[*counter] = atof( token );
+		printf("%f ",values[*counter]);
 		(*counter) = (*counter) +1;
 	}
-
+	printf("%d",*counter);
 	//printf( "\n" );
 	return values;
 
@@ -183,6 +196,41 @@ bool areCompatible( int firstColumn, int secondRow ) {
 
 bool haveCorrectNumberOfElements(int numberElementsExpected, int counter){
 	return ( counter == numberElementsExpected );
-
 }
 
+//sacado de http://stackoverflow.com/questions/12167946/how-do-i-read-an-arbitrarily-long-line-in-c
+
+char *getWholeLine(FILE *fp) {
+    char    *line = NULL;
+    size_t  maxlength = 0;
+    assert(fp != NULL);
+    for(;;) { // Read the line in BLOCKSIZE -blocks.
+        char *crlf, *block;
+
+        maxlength += BLOCKSIZE;
+        // This exploits realloc behaviour upon hitting NULL
+        if (NULL == (line = realloc(line, maxlength+1))) {
+            break; // realloc error returns NULL.
+        }
+        block = line + maxlength - BLOCKSIZE;
+        // BLOCKSIZE+1 to accommodate final zero
+        if (NULL == fgets(block, BLOCKSIZE+1, fp)) {
+            // TODO: rewind fp in case of error.
+            if (block == line) {
+                    // Error.
+                    free(line); line = NULL;
+            }
+            break;
+        }
+        // This was the last block iff we find a CRLF inside.
+        if (NULL != (crlf = strchr(block, '\n'))) {
+            *crlf = 0x0;
+            if (crlf != block) {
+                if ('\r' == *(--crlf))
+                    *crlf = 0x0;
+            }
+            break;
+        } /* if */
+    } /* for */
+    return line;
+}
